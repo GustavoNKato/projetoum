6a540a9012d1ade15502f10317827140ee3c9666
Gustavo Kato <gustavo.nkato@gmail.com>
2020-05-14 20:23:53 -0300

    Endpoint GET /pedidos/{id} criados

---
+++ src/main/java/com/kato/projetoum/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.kato.projetoum.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kato.projetoum.domain.Pedido;
+import com.kato.projetoum.repositories.PedidoRepository;
+import com.kato.projetoum.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
