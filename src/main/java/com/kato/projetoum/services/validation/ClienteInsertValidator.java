d639025e3144ffe996ccecd147df121d8af71135
Gustavo Kato <gustavo.nkato@gmail.com>
2020-05-18 21:03:33 -0300

    Validacao de e-mail para inserir um novo cliente

--- src/main/java/com/kato/projetoum/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/kato/projetoum/services/validation/ClienteInsertValidator.java
@@ -6,14 +6,22 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kato.projetoum.domain.Cliente;
 import com.kato.projetoum.domain.enums.TipoCliente;
 import com.kato.projetoum.dto.ClienteNewDTO;
+import com.kato.projetoum.repositories.ClienteRepository;
 import com.kato.projetoum.resources.exeception.FieldMessage;
 import com.kato.projetoum.services.validation.utils.BR;
 
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ é inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já cadastrado"));
 		}
 		
 		for (FieldMessage e : list) {
