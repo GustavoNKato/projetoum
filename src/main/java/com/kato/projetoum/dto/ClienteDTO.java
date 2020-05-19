d155b9d58bd3110312355f241aebd26e0ec819e6
Gustavo Kato <gustavo.nkato@gmail.com>
2020-05-18 22:21:04 -0300

    Validacao customizada de e-mail nao repetido na atualizacao de cliente

--- src/main/java/com/kato/projetoum/dto/ClienteDTO.java
+++ src/main/java/com/kato/projetoum/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.kato.projetoum.domain.Cliente;
+import com.kato.projetoum.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
