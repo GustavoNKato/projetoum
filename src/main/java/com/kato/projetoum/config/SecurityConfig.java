028d058e966ce3f47fb46542b578579473f141b0
Gustavo Kato <gustavo.nkato@gmail.com>
2020-06-28 23:27:33 -0300

    Implementando a autorizacao para usuarios

--- src/main/java/com/kato/projetoum/config/SecurityConfig.java
+++ src/main/java/com/kato/projetoum/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.kato.projetoum.security.JWTAuthenticationFilter;
+import com.kato.projetoum.security.JWTAuthorizationFilter;
 import com.kato.projetoum.security.JWTUtil;
 
 @Configuration
@@ -59,7 +60,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
 	}
 	
 	@Override
