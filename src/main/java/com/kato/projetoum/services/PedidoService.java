1150ae93a5e98f44f26c4818c8babc5a1589cec4
Gustavo Kato <gustavo.nkato@gmail.com>
2020-06-29 00:38:22 -0300

    Iserindo restricao de conteudo, cliente so recupera os seus pedidos

--- src/main/java/com/kato/projetoum/services/PedidoService.java
+++ src/main/java/com/kato/projetoum/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.kato.projetoum.domain.Cliente;
 import com.kato.projetoum.domain.ItemPedido;
 import com.kato.projetoum.domain.PagamentoComBoleto;
 import com.kato.projetoum.domain.Pedido;
@@ -15,6 +19,8 @@ import com.kato.projetoum.domain.enums.EstadoPagamento;
 import com.kato.projetoum.repositories.ItemPedidoRepository;
 import com.kato.projetoum.repositories.PagamentoRepository;
 import com.kato.projetoum.repositories.PedidoRepository;
+import com.kato.projetoum.security.UserSS;
+import com.kato.projetoum.services.exceptions.AuthorizationException;
 import com.kato.projetoum.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,5 +76,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
