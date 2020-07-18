34a7a3a7f3de0549bb2f212de16b8e005bb513de
Gustavo Kato <gustavo.nkato@gmail.com>
2020-07-18 17:36:03 -0300

    Enviando imagem pelo endPoint e tratando excecoes adequadamente

--- src/main/java/com/kato/projetoum/ProjetoumApplication.java
+++ src/main/java/com/kato/projetoum/ProjetoumApplication.java
@@ -1,18 +1,13 @@
 package com.kato.projetoum;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.kato.projetoum.services.S3Service;
 
 
 @SpringBootApplication
 public class ProjetoumApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoumApplication.class, args);
@@ -21,7 +16,6 @@ public class ProjetoumApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		s3Service.uploadFile("C:\\temp\\rayan.jpeg");
 	}
 
 }
--- src/main/java/com/kato/projetoum/services/ClienteService.java
+++ src/main/java/com/kato/projetoum/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.kato.projetoum.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kato.projetoum.domain.Cidade;
 import com.kato.projetoum.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		
@@ -109,4 +114,10 @@ public class ClienteService {
 		
 		return cli;
 	}
+	
+	public URI uploadProfilePricture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
+	
+	
 }
